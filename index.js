99d9f234e2dc0c36701163554b404691452e047b
Tauqueer Khan <tauqueer.khan@gmail.com>
2021-01-10 09:41:19 -0500

    🐛 FIX: fixed rover class

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 #! /usr/bin/env node
 
-const welcome = require('cli-welcome');
-const chalk = require('chalk');
 const path = require('path')
 
-const { validTextFile } = require('./utils/utils');
+const { validTextFile } = require('./utils/validTextFile');
 const textFileToJson = require('./utils/text-file-processor')
-// const { version, description } = require('./package.json');
 const roverManager = require('./utils/roverManager')
-// // Alerts
-// const success = chalk.green.inverse;
-// const info = chalk.blue.inverse;
-// const warning = chalk.keyword('orange').inverse;
-// const error = chalk.red.bold.inverse;
 
 const main = async () => {
 	if (process.argv.length < 2) {
